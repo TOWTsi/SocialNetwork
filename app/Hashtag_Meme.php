15a7e33790a3afcb76c4f1aa07d5f0d8a5eb06e2
TOWTsi <CorneliusTheodor.Vetesi@web.de>
2017-05-27 13:49:49 +0200

    Added tables for Hashtags an Hashtag_Memes

---
+++ app/Hashtag_Meme.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hashtag_Meme extends Model
+{
+    //
+    protected $fillable = [
+      'meme_id', 'hash_id',
+    ];
+}
