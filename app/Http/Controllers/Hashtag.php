bdcd4d103535af436722f69d908d8f4e71eb7f9f
TOWTsi <CorneliusTheodor.Vetesi@web.de>
2017-05-27 13:51:05 +0200

    Added Controller for Hashtags and Hashtag_Memes

---
+++ app/Http/Controllers/Hashtag.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Hashtag;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Storage;
+
+class Hashtag extends Controller
+{
+    //
+    public function store(Request $request)
+    {
+      $hashtag = new Hashtag;
+
+      $hashtag->name = $request->name;
+
+      $hashtag->save();
+    }
+}
---
+++ app/Http/Controllers/Hashtag_Meme.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Hashtag_Meme;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Storage;
+
+class Hashtag_Meme extends Controller
+{
+    //
+    public function store(Request $request)
+    {
+      $hashtag_meme = new Hashtag;
+
+      $hashtag_meme->meme_id = $request->meme_id;
+
+      $hashtag_meme->hash_id = $request->hash_id;
+
+      $hashtag_meme->save();
+    }
+}
